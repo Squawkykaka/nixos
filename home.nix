7efb13affde81998debbc717fc378b0d2eab7e64
Squawkykaka <squawkykaka@gmail.com>
2025-03-10 08:46:53 +1300

    Remove unneeded comment

--- home.nix
+++ home.nix
@@ -31,24 +31,5 @@
       theme = "catppuccin-mocha";
     };
   };
-
-  # programs.zsh = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   autosuggestion.enable = true;
-  #   syntaxHighlighting.enable = true;
-
-  #   shellAliases = {
-  #     ll = "ls -l";
-  #     update = "sudo nixos-rebuild switch";
-  #   };
-  #  history.size = 10000;
-
-  #   oh-my-zsh = {
-  #     enable = true;
-  #     plugins = [ "git" "battery" "thefuck" ];
-  #     theme = "robbyrussell";
-  #   };
-  # };
   home.stateVersion = "24.11";
 }
