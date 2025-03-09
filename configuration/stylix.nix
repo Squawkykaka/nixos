db11ba5dbeb217b9b9fe065110339904faf743cc
Squawkykaka <squawkykaka@gmail.com>
2025-03-10 10:40:16 +1300

    Add stuff

--- configuration/stylix.nix
+++ configuration/stylix.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }:
 {
   # stylix
-  stylix.enable = false;
+  stylix.enable = true;
+  stylix.autoEnable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   stylix.image = ../image.png;
 
@@ -9,4 +10,8 @@
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibita-Modern-Ice";
   stylix.cursor.size = 22;
+
+  # monospace font
+  stylix.fonts.monospace.package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+  stylix.fonts.monospace.name = "JetBrainsMonoNerdFontMono-Medium";
 }
