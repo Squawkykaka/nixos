f46d5255eb606f9226255a6ed356361647ed63e2
Squawkykaka <squawkykaka@gmail.com>
2025-03-11 09:52:48 +1300

    Stuff

--- configuration/plymouth.nix
+++ configuration/plymouth.nix
@@ -23,7 +23,12 @@
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
+      "intel_iommu=on"
+      "pcie_aspm=off"
     ];
+    
+    kernelModules = [ "kvm-intel" ];
+
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
