929c1e8c4a630bbcccdc00f542cbaac4ab0bd01e
Squawkykaka <squawkykaka@gmail.com>
2025-03-13 14:36:18 +1300

    add neovim config

--- homemanager/neovim.nix
+++ homemanager/neovim.nix
@@ -4,5 +4,15 @@
     enable = true;
     vimAlias = true;
     defaultEditor = true;
+    extraConfig = ''
+      set number relativenumber
+    '';
+
+    plugins = [
+      pkgs.vimPlugins.render-markdown-nvim
+      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+      pkgs.vimPlugins.coq_nvim
+      pkgs.vimPlugins.oil-nvim
+    ];
   };
 }
