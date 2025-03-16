52655f6e33290121e67ab60f99f82965fb86c2a6
Squawkykaka <squawkykaka@gmail.com>
2025-03-16 21:59:20 +1300

    add builders

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,21 @@
       ./configuration/apps/steam.nix
     ];
 
+  # nix builders
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "nixos-builder";
+        system = "x86_64-linux";
+        protocol = "ssh";
+        maxJobs = 4;  # Adjust based on your system capabilities
+        speedFactor = 1;
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      }
+    ];
+  };
+
   # delete those annoying files
   system.userActivationScripts = {
   removeConflictingFiles = {
