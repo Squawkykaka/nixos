23e71e281d90b0626a8cc87aec2cf505b007a398
Squawkykaka <squawkykaka@gmail.com>
2025-03-17 17:37:46 +1300

    add linux-zen

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Enable networking
   networking.hostName = "nix-squawkykaka"; # Define your hostname.
